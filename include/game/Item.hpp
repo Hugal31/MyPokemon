c9c69e1f2377292152145715aed9239d78d8846c
Hugal31 <hugo.laloge@epitech.eu>
2015-06-08 14:55:32 +0200

    Ajout des Items

--- include/game/Item.hpp
+++ include/game/Item.hpp
@@ -5,9 +5,8 @@
 ** Login   <quief_h@epitech.net>
 **
 ** Started on  Thu Jun  4 09:42:29 2015 Hippolyte QUIEF
-** Last update Thu Jun  4 10:53:59 2015 Hippolyte QUIEF
+** Last update Mon Jun  8 14:50:08 2015 Hugo Laloge
 */
-
 
 #ifndef		ITEM_H_
 # define	ITEM_H_
@@ -41,8 +40,8 @@ namespace	game
   protected:
     std::string		_name;
     std::string		_resum;
-    e_item_type		_type;
     unsigned int	_id;
+    e_item_type		_type;
     unsigned int	_buy_value;
     unsigned int	_sell_value;
 
@@ -54,31 +53,31 @@ namespace	game
       (void)version;
       ar & _name;
       ar & _resum;
-      ar & _type;
       ar & _id;
+      ar & _type;
       ar & _buy_value;
       ar & _sell_value;
     }
 
   public:
     /* Constructeur */
-    Item(unsigned int id) = 0;
-    virtual ~Item();
+    Item(unsigned int id);
+    virtual		~Item();
 
     /* Accesseurs */
     const std::string	&get_name() const;
     const std::string	&get_resum() const;
-    e_item_type		get_type() const;
     unsigned int	get_id() const;
+    e_item_type		get_type() const;
     unsigned int	get_buy_value() const;
     unsigned int	get_sell_value() const;
 
     /* Assesseurs */
     void		set_name(const std::string &name);
     void		set_resum(const std::string &resum);
-    void		set_type(const e_item_type &type);
-    void		set_buy_value(const unsiged int buy_value);
-    void		set_sell_value(const unsiged int sell_value);
+    void		set_type(e_item_type type);
+    void		set_buy_value(unsigned int buy_value);
+    void		set_sell_value(unsigned int sell_value);
   };
 
   std::ostream &operator<<(std::ostream &os, const Item &item);
--- src/game/Item/Item.cpp
+++ src/game/Item/Item.cpp
@@ -5,13 +5,13 @@
 ** Login   <quief_h@epitech.net>
 **
 ** Started on  Thu Jun  4 10:35:57 2015 Hippolyte QUIEF
-** Last update Thu Jun  4 10:55:23 2015 Hippolyte QUIEF
+** Last update Mon Jun  8 14:52:05 2015 Hugo Laloge
 */
-
 
 #include	<ostream>
 #include	<string>
 #include	"game/PokemonModele.hpp"
+#include	"game/Item.hpp"
 
 using namespace	game;
 
@@ -40,24 +40,26 @@ const std::string	&Item::get_resum() const
   return (_resum);
 }
 
-e_item_type		&Item::get_type() const
+e_item_type		Item::get_type() const
 {
   return (_type);
 }
 
-unsigned int		&Item::get_id() const
+unsigned int		Item::get_id() const
 {
   return (_id);
 }
 
-unsigned int		&Item::get_buy_value() const
+unsigned int		Item::get_buy_value() const
 {
   return (_buy_value);
 }
-unsigned int		&Item::get_sell_value() const
+
+unsigned int		Item::get_sell_value() const
 {
   return (_sell_value);
 }
+
 /*
 ** Assesseurs
 */
@@ -72,22 +74,17 @@ void		Item::set_resum(const std::string &resum)
   _resum = resum;
 }
 
-void		Item::set_type(e_item_type &type)
+void		Item::set_type(e_item_type type)
 {
   _type = type;
 }
 
-void		Item::set_id(unsigned int &id)
-{
-  _id = id;
-}
-
-void		Item::set_buy_value(unsigned int &buy_value)
+void		Item::set_buy_value(unsigned int buy_value)
 {
   _buy_value = buy_value;
 }
 
-void		Item::set_sell_value(unsigned int &sell_value)
+void		Item::set_sell_value(unsigned int sell_value)
 {
   _sell_value = sell_value;
 }
@@ -96,12 +93,13 @@ void		Item::set_sell_value(unsigned int &sell_value)
 ** Surcharge externes
 */
 
-std::ostream	&game::operator<<(std::ostream &os, const PokemonModele &pokemon)
+std::ostream	&game::operator<<(std::ostream &os, const Item &item)
 {
-  os << "Id : " << pokemon.get_id() << std::endl
-     << "Name : " << pokemon.get_name() << std::endl
-     << "Resum : " << pokemon.get_resum() << std::endl
-     << "Type : " << pokemon.get_type() << std::endl
-     << "Buy Value : " << pokemon.get_buy_value() << std::endl
-     << "Sell Value : " << pokemon.get_sell_value() << std::endl;
+  os << "Id : " << item.get_id() << std::endl
+     << "Name : " << item.get_name() << std::endl
+     << "Resum : " << item.get_resum() << std::endl
+     << "Type : " << item.get_type() << std::endl
+     << "Buy Value : " << item.get_buy_value() << std::endl
+     << "Sell Value : " << item.get_sell_value() << std::endl;
   return (os);
+}
